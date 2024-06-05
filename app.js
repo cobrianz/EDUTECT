6ae185a349f4831933a03be47212db265c95a9bf
cobrianz <briancheruiyot.h@gmail.com>
2024-06-05 13:37:15 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function toggleSubtopics(element) {
+    const subtopics = element.querySelector('.subtopics');
+    const icon = element.querySelector('i');
+    if (subtopics.style.display === 'none' || subtopics.style.display === '') {
+        subtopics.style.display = 'block';
+        icon.classList.remove('fa-chevron-down');
+        icon.classList.add('fa-chevron-up');
+    } else {
+        subtopics.style.display = 'none';
+        icon.classList.remove('fa-chevron-up');
+        icon.classList.add('fa-chevron-down');
+    }
+}
+
+function toggleAside() {
+    var aside = document.getElementById("courseOutline");
+    aside.classList.toggle("active");
+}
