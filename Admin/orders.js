57c736e5466d84adf63998c687e7861a606788f5
cobrianz <briancheruiyot.h@gmail.com>
2024-06-07 17:00:27 +0300

    first commit

---
+++ Admin/orders.js
@@ -0,0 +1,92 @@
+const Orders = [
+    {
+      orderName: "Nursing Research Paper",
+      orderNumber: "85631",
+      paymentStatus: "Due",
+      shipping: "Pending"
+    },
+    {
+      orderName: "History Essay",
+      orderNumber: "85632",
+      paymentStatus: "Paid",
+      shipping: "Declined"
+    },
+    {
+      orderName: "Math Homework",
+      orderNumber: "85633",
+      paymentStatus: "Due",
+      shipping: "Pending"
+    },
+    {
+      orderName: "English Literature Review",
+      orderNumber: "85634",
+      paymentStatus: "Due",
+      shipping: "Pending"
+    },
+    {
+      orderName: "Chemistry Lab Report",
+      orderNumber: "85635",
+      paymentStatus: "Paid",
+      shipping: "Declined"
+    },
+    {
+      orderName: "Physics Homework",
+      orderNumber: "85636",
+      paymentStatus: "Due",
+      shipping: "Pending"
+    },
+    {
+      orderName: "Computer Science Project",
+      orderNumber: "85637",
+      paymentStatus: "Paid",
+      shipping: "Declined"
+    },
+    {
+      orderName: "Biology Research Paper",
+      orderNumber: "85638",
+      paymentStatus: "Due",
+      shipping: "Pending"
+    },
+    {
+      orderName: "Marketing Case Study",
+      orderNumber: "85639",
+      paymentStatus: "Paid",
+      shipping: "Declined"
+    },
+    {
+      orderName: "Statistics Assignment",
+      orderNumber: "85640",
+      paymentStatus: "Due",
+      shipping: "Pending"
+    },
+    {
+      orderName: "Nursing Research Paper",
+      orderNumber: "85631",
+      paymentStatus: "Due",
+      shipping: "Pending"
+    },
+    {
+      orderName: "History Essay",
+      orderNumber: "85632",
+      paymentStatus: "Paid",
+      shipping: "Declined"
+    },
+    {
+      orderName: "Math Homework",
+      orderNumber: "85633",
+      paymentStatus: "Due",
+      shipping: "Pending"
+    },
+    {
+      orderName: "English Literature Review",
+      orderNumber: "85634",
+      paymentStatus: "Due",
+      shipping: "Pending"
+    },
+    {
+      orderName: "Chemistry Lab Report",
+      orderNumber: "85635",
+      paymentStatus: "Paid",
+      shipping: "Declined"
+    }
+  ];
